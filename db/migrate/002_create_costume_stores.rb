36ef205cd574fb556556f65e200c708bb0f8decb
Marc Poppy <poppy.marc@gmail.com>
2020-01-09 05:10:18 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,4 +1,15 @@
 # Create your costume_stores migration here
 
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees
+      t.boolean :in_business
+      t.timestamps
+    end
+
+  end
 end
