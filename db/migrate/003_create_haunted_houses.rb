9aadb01011921091bc30fb9d8edbb61c494f5d3d
Marc Poppy <poppy.marc@gmail.com>
2020-01-09 05:22:15 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouse < ActiveRecord::Migration
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :friendly_or_not
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :long_description
+    end
+  end
+end
