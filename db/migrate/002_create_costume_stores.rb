af7dc89cef55d30e0304617916e6d2a92cd6d974
Marc Poppy <poppy.marc@gmail.com>
2020-01-09 05:14:23 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,7 +8,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.integer :costume_inventory
       t.integer :num_employees
       t.boolean :still_in_business
-      t.timestamps
+      t.string :opening_time
+      t.string :closing_time
     end
 
   end
