d6b8fb56a6475d6b0663ca28dc523284788f60e7
Marc Poppy <poppy.marc@gmail.com>
2020-01-09 05:04:46 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,6 +11,11 @@
 class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.string :size
+      t.string :image_url
+      t.timestamp
     end
   end
 end
